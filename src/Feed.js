5c27717c271acf59cca9f7b9503b6a2d9b491344
Magda <magdagozdalik28@gmail.com>
2022-02-17 14:14:46 +0000

    Feed

---
+++ src/Feed.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from "react";
+import "./Feed.css";
+import CreateIcon from "@material-ui/icons/Create";
+import ImageIcon from "@material-ui/icons/Image";
+import InputOption from "./InputOption.js";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+import Post from "./Post.js";
+import { db } from "./firebase.js";
+import firebase from "firebase/compat/app";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import { cleanup } from "@testing-library/react";
+import FlipMove from "react-flip-move";
+
+function Feed() {
+  const user = useSelector(selectUser);
+  const [input, setInput] = useState("");
+  const [posts, setPost] = useState([]);
+
+  //retrieve docs from firebase data collection into the posts state
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPost(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+
+    return () => {
+      cleanup;
+    };
+  }, []);
+
+  const sendPost = (e) => {
+    e.preventDefault();
+    db.collection("posts").add({
+      name: user.displayName,
+      description: user.email,
+      message: input,
+      photoUrl: user.photoUrl || "",
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setInput("");
+  };
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form action="">
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              type="text"
+            />
+            <button onClick={sendPost} type="submit">
+              Send
+            </button>
+          </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write article"
+            color="#7FC15E"
+          />
+        </div>
+      </div>
+
+      <div>
+        {/* wrap your parent component in FlipMove  */}
+        <FlipMove>
+          {posts.map(
+            ({ id, data: { name, description, message, photoUrl } }) => (
+              <Post
+                key={id}
+                name={name}
+                description={description}
+                message={message}
+                photoUrl={photoUrl}
+              />
+            )
+          )}
+        </FlipMove>
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
